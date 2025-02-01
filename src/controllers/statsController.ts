a80c13092fe33db6b1c5f4d2c295e765b112b86c
henri-hietala <henri.hietala@futurice.com>
2025-02-01 14:14:04 +0200

    feat: filter queries

--- src/controllers/statsController.ts
+++ src/controllers/statsController.ts
@@ -6,13 +6,64 @@ import {
 import { electricitydata } from "@prisma/client";
 import { ElectricityDataDTO } from "../models/dataTransferObjects";
 
+export type FlatFilter = {
+  [key in keyof electricitydata]: (string | null)[];
+};
+
+export type Sorting = {
+  id: keyof electricitydata;
+  desc: boolean;
+};
+
+export type QueryParams = {
+  start: number;
+  size: number;
+  filters: FlatFilter;
+  globalFilter: string;
+  sorting: Sorting[];
+};
+
+const exampleQuery = {
+  start: "0",
+  size: "10",
+  filters:
+    '[{"id":"id","value":[null,"12"]},{"id":"date","value":["2025-02-18T22:00:00.000Z",null]},{"id":"starttime","value":["2025-02-01T02:00:00.000Z","2025-02-01T01:00:00.000Z"]},{"id":"productionamount","value":["1","9"]},{"id":"consumptionamount","value":["1",null]},{"id":"hourlyprice","value":[null,null]}]',
+  globalFilter: "",
+  sorting: "[]",
+};
+
+/**
+ * convert id:s to key of electricitydata
+ */
+const handleParseFilters = (filters: string) => {
+  if (!filters) {
+    return {} as FlatFilter;
+  }
+  const parsedFilters: FlatFilter = JSON.parse(filters).reduce(
+    (acc: FlatFilter, filter: { id: string; value: (string | null)[] }) => {
+      acc[filter.id as keyof electricitydata] = filter.value;
+      return acc;
+    }
+  );
+
+  return parsedFilters;
+};
+
 export const getDailyStats = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const stats: electricitydata[] = await getDailyStatsService();
+    const queryParams = {
+      start: parseInt(req.query.start as string),
+      size: parseInt(req.query.size as string),
+      filters: handleParseFilters(req.query.filters as string),
+      globalFilter: req.query.globalFilter as string,
+      sorting: JSON.parse(req.query.sorting as string) as Sorting[],
+    };
+
+    const stats: electricitydata[] = await getDailyStatsService(queryParams);
     const response: ElectricityDataDTO = {
       data: stats,
       // TODO - implement pagination
--- src/services/statsService.ts
+++ src/services/statsService.ts
@@ -1,9 +1,61 @@
-import { electricitydata, PrismaClient } from "@prisma/client";
+import { electricitydata, Prisma, PrismaClient } from "@prisma/client";
+import { FlatFilter, QueryParams } from "../controllers/statsController";
 
 const prisma = new PrismaClient();
 
-export const getDailyStatsService = async (): Promise<electricitydata[]> => {
-  return await prisma.electricitydata.findMany();
+export const buildWhereClause = (
+  filters: FlatFilter
+): Prisma.electricitydataWhereInput => {
+  console.log("params", filters);
+  // TODO UTC handling
+  const whereClause: Prisma.electricitydataWhereInput = {
+    date: {
+      gte: filters.date[0] ? new Date(filters.date[0]) : undefined,
+      lte: filters.date[1] ? new Date(filters.date[1]) : undefined,
+    },
+    starttime: {
+      gte: filters.starttime[0] ? new Date(filters.starttime[0]) : undefined,
+      lte: filters.starttime[1] ? new Date(filters.starttime[1]) : undefined,
+    },
+    productionamount: {
+      gte: filters.productionamount[0]
+        ? parseFloat(filters.productionamount[0])
+        : undefined,
+      lte: filters.productionamount[1]
+        ? parseFloat(filters.productionamount[1])
+        : undefined,
+    },
+    consumptionamount: {
+      gte: filters.consumptionamount[0]
+        ? parseFloat(filters.consumptionamount[0])
+        : undefined,
+      lte: filters.consumptionamount[1]
+        ? parseFloat(filters.consumptionamount[1])
+        : undefined,
+    },
+    hourlyprice: {
+      gte: filters.hourlyprice[0]
+        ? parseFloat(filters.hourlyprice[0])
+        : undefined,
+      lte: filters.hourlyprice[1]
+        ? parseFloat(filters.hourlyprice[1])
+        : undefined,
+    },
+    /*
+    id: {
+      in: filters.id.filter((id) => id !== null).map((id) => parseInt(id)),
+    },*/
+  };
+
+  console.log("whereclause", whereClause);
+  return whereClause;
+};
+export const getDailyStatsService = async (
+  queryParams: QueryParams
+): Promise<electricitydata[]> => {
+  return await prisma.electricitydata.findMany({
+    where: buildWhereClause(queryParams.filters),
+  });
 };
 
 export const getStatsByDateService = async (date: string) => {
