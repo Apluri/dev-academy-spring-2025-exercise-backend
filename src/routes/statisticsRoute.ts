1bd47d1c8e769dbfecebae2dfa40d1c342e17daa
henri-hietala <henri.hietala@futurice.com>
2025-02-04 18:41:48 +0200

    refactor: remove unused features

--- src/routes/statisticsRoute.ts
+++ src/routes/statisticsRoute.ts
@@ -1,12 +1,8 @@
 import { Router } from "express";
-import {
-  getRawDataTemp,
-  handleGetDailyStatistics,
-} from "../controllers/statisticsController";
+import { handleGetDailyStatistics } from "../controllers/statisticsController";
 
 const router = Router();
 
-router.get("/raw", getRawDataTemp); // TODO: remove this route
 router.get("/daily", handleGetDailyStatistics);
 
 export default router;
